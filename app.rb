55a3380cc03beea0f69cf42a9580a05625aecf52
charliefea <charlotte.feather@yahoo.com>
2017-05-22 16:02:00 +0100

    Add more pages and Shotgun gem

--- app.rb
+++ app.rb
@@ -3,3 +3,23 @@ require 'sinatra'
 get '/' do
   "Hello World"
 end
+
+get '/secret' do
+  "Charlotte and Jade were here!"
+end
+
+get '/test' do
+  'Test 123 test 123'
+end
+
+get '/jade' do
+  "she is awesome"
+end
+
+get '/charlotte' do
+  "Is not quite as awesome as Jade"
+end
+
+get '/makers' do
+  "Why is it so difficult"
+end
