501b38cab4849718cdba3b56c7e2c22747e5b811
Guillermo <mrog0@outlook.com>
2017-05-23 14:40:01 +0100

    post params

--- app.rb
+++ app.rb
@@ -5,33 +5,17 @@ get '/' do
   "Hello World"
 end
 
-get '/secret' do
-  "Charlotte and Jade were here!"
-end
-
-get '/test' do
-  'Test 123 test 123'
-end
-
-get '/jade' do
-  "she is awesome"
-end
-
-get '/charlotte' do
-  "Is not quite as awesome as Jade"
-end
-
-get '/makers' do
-  "Why is it so difficult"
-end
-
 get '/random-cat' do
   @name = ["pollo", "chicken", "baby"].sample
   erb(:index)
 end
 
-get '/name-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
+end
+
+get '/cat-form' do
+  erb (:cat_form)
 end
